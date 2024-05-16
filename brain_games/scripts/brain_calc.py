b5652eec29139e8ce8682f9a2ede30746860835d
“VG” <lolmanip201289@gmail.com>
2024-05-16 23:50:26 +0400

    asfwfadefvrff

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -2,14 +2,6 @@ import random
 import prompt
 
 
-def random_num1():
-    return random.randint(30, 99)
-
-
-def random_num2():
-    return random.randint(1, 30)
-
-
 def main():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
@@ -17,16 +9,18 @@ def main():
     print('What is the result of the expression?')
     correct_answers_count = 0
     while correct_answers_count < 3:
+        random_num1 = random.randint(30, 99)
+        random_num2 = random.randint(1, 30)
         random_sign = random.choice(['+', '-', '*'])
-        print(f'Question: {random_num1()} {random_sign} {random_num2()}')
+        print(f'Question: {random_num1} {random_sign} {random_num2}')
         user_answer = prompt.string('Your answer: ')
-        if random_sign == '+':
-            answer = random_num1() + random_num2()
-        elif random_sign == '-':
-            answer = random_num1() - random_num2()
-        elif random_sign == '*':
-            answer = random_num1() * random_num2()
-
+        match random_sign:
+            case '+':
+                answer = random_num1 + random_num2
+            case '-':
+                answer = random_num1 - random_num2
+            case '*':
+                answer = random_num1 * random_num2
         if int(user_answer) == answer:
             print('Correct!')
             correct_answers_count += 1
