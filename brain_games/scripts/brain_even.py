2049b3be6129460f1f7130fbc565f9b7f70c9d1a
“VG” <lolmanip201289@gmail.com>
2024-05-16 19:13:22 +0400

    ;

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -8,20 +8,23 @@ def main():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print('Answer "yes" if the number is even, otherwise answer "no".')
+
     correct_answers_count = 0
     while correct_answers_count < 3:
         random_num = random.randint(1, 100)
         print(f'Question: {random_num}')
         user_answer = prompt.string('Your answer: ')
-        if random_num % 2 == 0 and user_answer == 'yes':
+
+        if random_num % 2 == 0:
             answer = 'yes'
-        elif random_num % 2 != 0 and user_answer == 'no':
+        else:
             answer = 'no'
 
         if user_answer == answer:
             print('Correct!')
             correct_answers_count += 1
         else:
+            correct_answers_count = 0
             print(f"{user_answer} is wrong answer ;(. Correct answer was {answer}.\nLet's try again, {name}!")
 
     print(f'Congratulations, {name}!')
