86288b84b8ec6771b4dfc694494fb32d2ed68ec3
“VG” <lolmanip201289@gmail.com>
2024-05-16 21:39:09 +0400

    asfasfda

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -12,7 +12,7 @@ def main():
         start_num = random.randint(1, 3)
         step_num = random.randint(1, 3)
         random_numbers = [_ for _ in range(start_num, 20, step_num)]
-        random_marker = random.randint(0, len(random_numbers))
+        random_marker = random.randint(0, len(random_numbers) - 1)
         random_numbers[random_marker] = '..'
         numbers_string = ' '.join(map(str, random_numbers))
         print(f'What number is missing in the progression? {numbers_string}')
