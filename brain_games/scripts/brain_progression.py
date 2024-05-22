d5909f20a1877081feac0a97fa97827ade4e8b79
“VG” <lolmanip201289@gmail.com>
2024-05-23 01:38:53 +0400

    hope die in the end

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import random
 import prompt
-from brain_games.generate_progression import make_question
+from brain_games.generate_progression import make_question, generate_progression
 
 
 def main():
@@ -10,15 +10,20 @@ def main():
     print(f'Hello, {name}!')
     correct_answers_count = 0
     while correct_answers_count < 3:
+        start_num = random.randint(1, 3)
+        step_num = random.randint(1, 3)
+        progression = generate_progression(start_num, step_num)
+        question, place_marker = make_question(progression)
         print('What number is missing in the progression?')
-        print(f'Question: {make_question()}')
+        print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
-        if int(user_answer) == make_question():
+        answer = place_marker * step_num + start_num
+        if int(user_answer) == answer:
             print('Correct!')
             correct_answers_count += 1
         else:
             print(f"{user_answer} is wrong answer ;(. Correct answer was "
-                  f"{make_question()}.\nLet's try again, {name}!")
+                  f"{answer}.\nLet's try again, {name}!")
             break
     if correct_answers_count == 3:
         print(f'Congratulations, {name}!')
