96866779f03f8e77919c8c5d03f8594cccf38e9e
“VG” <lolmanip201289@gmail.com>
2024-05-23 15:46:49 +0400

    fix progression

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -11,13 +11,12 @@ def main():
     correct_answers_count = 0
     while correct_answers_count < 3:
         start_num = random.randint(1, 3)
-        step_num = random.randint(1, 3)
+        step_num = random.randint(1, 4)
         progression = generate_progression(start_num, step_num)
-        question, place_marker = make_question(progression)
+        question, answer = make_question(progression)
         print('What number is missing in the progression?')
         print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
-        answer = place_marker * step_num + start_num
         if int(user_answer) == answer:
             print('Correct!')
             correct_answers_count += 1
